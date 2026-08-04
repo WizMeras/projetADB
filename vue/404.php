8de19b655de79ac97ce69e52dbaf0f27c077e0da
David <david.vermersch63@gmail.com>
2026-08-04 16:02:51 +0200

    Création page 404

---
+++ vue/404.php
@@ -0,0 +1,21 @@
+<?php
+include("header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 - PenguinWatch</title>
+</head>
+<body>
+    <div class="container">
+        <div class="d-flex flex-column justify-content-center align-items-center" style="height: 90vh;">
+            <h1>404</h1>
+            <h2>La page n'a pas été trouvé</h2>
+            <a class="btn btn-primary mt-3" href="index.php?page=accueil">Revenir à l'accueil</a>
+        </div>
+    </div>
+</body>
+</html>
